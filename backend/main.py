c920e8a0b8344d72c3f102ee8c8d0831ac05e5f8
Artyom Grislis <107166791+agrislis@users.noreply.github.com>
2026-03-09 23:39:39 +0300

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from .database import engine
+from .models import Base
+from .routers import prompts, users
+
+app = FastAPI()
+
+# Создание таблиц БД
+Base.metadata.create_all(bind=engine)
+
+app.include_router(users.router)
+app.include_router(prompts.router)
